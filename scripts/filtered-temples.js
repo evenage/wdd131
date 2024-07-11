bc05d0f0988994ebcf76f510b4133ddf03eb34e5
evenage <charue.vyago2019@gmail.com>
2024-07-11 23:22:19 +0200

    change

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,81 +1,66 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const currentYear = new Date().getFullYear();
-    document.getElementById("currentYear").textContent= currentYear;
-
-    document.getElementById("lastModified").textContent= "Last Modified: " + document.lastModified;
-
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('nav-menu');
-
-    hamburger.addEventListener('click', () => {
-        navMenu.classList.toggle('show');
-        // Toggle between hamburger and 'X' icon
-        if (hamburger.textContent === '\u2630') {
-            hamburger.textContent = '\u2715';
-        } else {
-            hamburger.textContent = '\u2630';
-        }
-    });
-});
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav');
+const album = document.querySelector('#temple-album')
+const homeButton = document.querySelector(".home");
+const oldButton = document.querySelector(".old");
+const newButton = document.querySelector(".new");
+const largeButton = document.querySelector(".large");
+const smallButton = document.querySelector(".small");
+const pageTitle = document.querySelector('#title');
 
 const temples = [
-    {
-      templeName: "Aba Nigeria",
-      location: "Aba, Nigeria",
-      dedicated: "2005, August, 7",
-      area: 11500,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
-    },
-    {
-      templeName: "Manti Utah",
-      location: "Manti, Utah, United States",
-      dedicated: "1888, May, 21",
-      area: 74792,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
-    },
-    {
-      templeName: "Payson Utah",
-      location: "Payson, Utah, United States",
-      dedicated: "2015, June, 7",
-      area: 96630,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
-    },
-    {
-      templeName: "Yigo Guam",
-      location: "Yigo, Guam",
-      dedicated: "2020, May, 2",
-      area: 6861,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
-    },
-    {
-      templeName: "Washington D.C.",
-      location: "Kensington, Maryland, United States",
-      dedicated: "1974, November, 19",
-      area: 156558,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
-    },
-    {
-      templeName: "Lima Perú",
-      location: "Lima, Perú",
-      dedicated: "1986, January, 10",
-      area: 9600,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
-    },
-    {
-      templeName: "Mexico City Mexico",
-      location: "Mexico City, Mexico",
-      dedicated: "1983, December, 2",
-      area: 116642,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
-    },
-    {
+	{
+		templeName: "Aba Nigeria",
+		location: "Aba, Nigeria",
+		dedicated: "2005, August, 7",
+		area: 11500,
+		imgWidth: 399,
+		imgHieght: 250,
+		imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+	},
+	{
+		templeName: "Manti Utah",
+		location: "Manti, Utah, United States",
+		dedicated: "1888, May, 21",
+		area: 74792,
+		imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+	},
+	{
+		templeName: "Payson Utah",
+		location: "Payson, Utah, United States",
+		dedicated: "2015, June, 7",
+		area: 96630,
+		imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+	},
+	{
+		templeName: "Yigo Guam",
+		location: "Yigo, Guam",
+		dedicated: "2020, May, 2",
+		area: 6861,
+		imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+	},
+	{
+		templeName: "Washington D.C.",
+		location: "Kensington, Maryland, United States",
+		dedicated: "1974, November, 19",
+		area: 156558,
+		imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+	},
+	{
+		templeName: "Lima Perú",
+		location: "Lima, Perú",
+		dedicated: "1986, January, 10",
+		area: 9600,
+		imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+	},
+	{
+		templeName: "Mexico City Mexico",
+		location: "Mexico City, Mexico",
+		dedicated: "1983, December, 2",
+		area: 116642,
+		imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+	},
+	{
         templeName: "Adelaide",
         location: "South Australia",
         dedicated: "15 June 2000",
@@ -95,75 +80,75 @@ const temples = [
         templeName:"Bentonville",
         location: "Arkansas",
         dedication: "17 september 2023",
-        area: 25000 ,
+		area: 25000 ,
         imageUrl: 
         "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/bentonville-arkansas/400x400/34410870d7d011ed8402eeeeac1ec672bdc0ce45.jpeg"
     },
-
 ];
 
-createTempleCard();
 
-function createTempleCard(temple) {
-    let card = document.createElement("div");
-    card.classList.add("temple-card");
-    let name = document.createElement("h3");
-    name.textContent = temple.name;
+function buildTempleAlbum(filter = temples) {
+	album.innerHTML = ""
 
-    let location = document.createElement("p");
-    location.textContent = `Location: ${temple.location}`;
+	filter.forEach(temple => {
+		const name = temple.templeName;
+		const location = temple.location;
+		const dedicated = temple.dedicated;
+		const area = temple.area;
+		const image = temple.imageUrl;
+		const card = document.createElement('figure');
+    })
+};
 
-    let dedication = document.createElement("p");
-    dedication.textContent = `Dedicated: ${temple.dedication}`;
+card.innerHTML = <div><h3>${name}</h3><figcaption>Location:<span> ${location}</span>
+</figcaption><figcaption>Dedicated:<span> ${dedicated}</span></figcaption>
+<figcaption>Area:<span> ${area} sq ft</span></figcaption>
+<img src="${image}" alt="the ${name} Temple"
+width="${imgWidth}" height="${imgHieght}" loading="lazy"/>; album.appendChild(card);`</div>
 
-    let area = document.createElement("p");
-    area.textContent = `Area: ${temple.area} sq ft`;
+buildTempleAlbum();
 
-    let img = document.createElement("img");
-    img.src = temple.image;
-    img.alt = `${temple.name}`;
-    img.loading = "lazy";
+hamButton.addEventListener('click', ()=> {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
 
-    let overlay = document.createElement("div");
-    overlay.classList.add("overlay");
+homeButton.addEventListener('click', () => {
+	document.querySelector('.active').classList.toggle('active');
+	pageTitle.textContent = 'Home';
+	homeButton.classList.toggle('active');
 
-    let overlayText = document.createElement("div");
-    overlayText.classList.add("overlay-text");
-    overlayText.innerHTML = `
-        <p><strong>${temple.name}</strong></p>
-        <p>${temple.location}</p>
-        <p>Dedicated: ${temple.dedication}</p>
-        <p>Area: ${temple.area} sq ft</p>
-    `;
+	buildTempleAlbum(temples);
+})
 
-    overlay.appendChild(overlayText);
-    imgContainer.appendChild(img);
-    imgContainer.appendChild(overlay);
-    card.appendChild(imgContainer);
-    document.querySelector(".res-grid").appendChild(card);
-}
+oldButton.addEventListener('click', () => {
+	document.querySelector('.active').classList.toggle('active');
+	pageTitle.textContent = 'Old';
+	oldButton.classList.toggle('active');
 
+	buildTempleAlbum(temples.filter(temple => temple.dedicated.split(',')[0] < 1900));
+})
 
-    card.appendChild(name);
-    card.appendChild(location);
-    card.appendChild(dedication);
-    card.appendChild(area);
-    card.appendChild(img);
+newButton.addEventListener('click', () => {
+	document.querySelector('.active').classList.toggle('active');
+	pageTitle.textContent = 'New';
+	newButton.classList.toggle('active');
 
-    document.querySelector(".res-grid").appendChild(card);
+	buildTempleAlbum(temples.filter(temple => temple.dedicated.split(',')[0] > 2000));
+})
 
+largeButton.addEventListener('click', () => {
+	document.querySelector('.active').classList.toggle('active');
+	pageTitle.textContent = 'Large';
+	largeButton.classList.toggle('active');
 
-function displayTemples(filterFunction) {
-    const grid = document.querySelector(".res-grid");
-    grid.innerHTML = ""; // Clear previous temples
-    temples.filter(filterFunction).forEach(createTempleCard);
-}
+	buildTempleAlbum(temples.filter(temple => temple.area > 90000));
+})
 
-document.querySelector(".nav-home").addEventListener("click", () => displayTemples(() => true));
-document.querySelector(".nav-old").addEventListener("click", () => displayTemples(temple => new Date(temple.dedication) < new Date('1900-01-01')));
-document.querySelector(".nav-new").addEventListener("click", () => displayTemples(temple => new Date(temple.dedication) > new Date('2000-01-01')));
-document.querySelector(".nav-large").addEventListener("click", () => displayTemples(temple => temple.area > 90000));
-document.querySelector(".nav-small").addEventListener("click", () => displayTemples(temple => temple.area < 10000));
+smallButton.addEventListener('click', () => {
+	document.querySelector('.active').classList.toggle('active');
+	pageTitle.textContent = 'Small';
+	smallButton.classList.toggle('active');
 
-// Initially display all temples
-displayTemples(() => true);
+	buildTempleAlbum(temples.filter(temple => temple.area < 10000));
+})
