687d829b7f5143647c1d1c1fbb2515aa1be9371e
evenage <charue.vyago2019@gmail.com>
2024-07-17 00:49:36 +0200

    changes

---
+++ project/project-scripts/siteplan.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const siteNameElement = document.getElementById('site-name');
+    siteNameElement.addEventListener('click', () => {
+        alert('Welcome to ArtSpace! Your gateway to the art world.');
+    });
+});
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
